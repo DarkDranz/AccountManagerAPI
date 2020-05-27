686fb14dbfdc80a74204cf200332a766bb9d72ed
DarkDranz <patrick96@hotmail.fr>
2020-05-27 18:52:55 +0200

    news

---
+++ AccountManagerAPI/Controllers/FileController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountManagerAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public FileController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        // download file(s) to client according path: rootDirectory/subDirectory with single zip file
+        [HttpGet("Download/{subDirectory}")]
+        public IActionResult DownloadFiles(string subDirectory)
+        {
+            try
+            {
+                var (fileType, archiveData, archiveName) = _fileService.FetechFiles(subDirectory);
+
+                return File(archiveData, fileType, archiveName);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest($"Error: {exception.Message}");
+            }
+        }
+
+        // upload file(s) to server that palce under path: rootDirectory/subDirectory
+        [HttpPost("upload")]
+        public IActionResult UploadFile([FromForm(Name = "files")] List<IFormFile> files, string subDirectory)
+        {
+            try
+            {
+                _fileService.SaveFile(files, subDirectory);
+
+                return Ok(new { files.Count, Size = IFileService.SizeConverter(files.Sum(f => f.Length)) });
+            }
+            catch (Exception exception)
+            {
+                return BadRequest($"Error: {exception.Message}");
+            }
+        }
+    }
+}
+
